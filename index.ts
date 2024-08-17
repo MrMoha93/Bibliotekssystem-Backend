313557f4cadacad4c14b78d08ef707cdf8552ec1
MohaK <mohakhalili@outlook.com>
2024-08-17 20:19:28 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/api/test", (req, res) => {
+  return res.send("My first API endpoint");
+});
+
+const PORT = 5588;
+
+app.listen(PORT, () => console.log("Listening on port 5588" + PORT));
+
+// Intensive Foods Backend - Array in memory 07:00
