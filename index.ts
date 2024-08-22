2dfb39d172567d8b5d3ecd831c8c118a19bb4758
MohaK <mohakhalili@outlook.com>
2024-08-22 17:26:51 +0200

    installed cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import categories from "./routes/categories";
 import items from "./routes/items";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/items", items);
