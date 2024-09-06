313bdc6f43031adf5bd3439c99b70bd4b0dbc2ed
MohaK <mohakhalili@outlook.com>
2024-09-06 15:30:23 +0200

    fixed cors again

--- index.ts
+++ index.ts
@@ -5,7 +5,15 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5588",
+      "http://localhost:4173",
+      "https://bibliotekssystem-frontend3.onrender.com",
+    ],
+  })
+);
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/items", items);
